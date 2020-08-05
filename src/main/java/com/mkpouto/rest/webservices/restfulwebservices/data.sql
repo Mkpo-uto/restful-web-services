4da0db9606cc5639810c12eb29549c063e16bc1d
mkpou <mkpoutoe@hotmail.com>
2020-08-05 10:42:23 +1000

    Initial commit

---
+++ src/main/java/com/mkpouto/rest/webservices/restfulwebservices/data.sql
@@ -0,0 +1,5 @@
+insert into user values (100001, sysdate(),'AB');
+insert into user values (100002, sysdate(),'Jam');
+insert into user values (100003, sysdate(),'Jill');
+insert into post values (11001, 'My First Post',100001);
+insert into post values (11002, 'My Second Post',100001);
---
+++ src/main/java/com/mkpouto/rest/webservices/restfulwebservices/hello/HelloWorldController.java
@@ -0,0 +1,36 @@
+package com.mkpouto.rest.webservices.restfulwebservices.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @RequestMapping(method= RequestMethod.GET, path="/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @RequestMapping(method= RequestMethod.GET, path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+    //hello-world/path-variable/edet
+    @RequestMapping(method= RequestMethod.GET, path="/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @RequestMapping(method= RequestMethod.GET, path="/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
+    }
+}
